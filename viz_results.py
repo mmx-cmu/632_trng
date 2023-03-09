8e23ec8424605712da2b6cf79dad131840b4e8ad
Sebastian <sbernal@andrew.cmu.edu>
2023-03-09 11:30:16 -0800

    Post-processing files print out the percent of 1s in the binary stream

--- viz_results.py
+++ viz_results.py
@@ -1,7 +1,10 @@
+import sys
 from PIL import Image
 from math import sqrt, ceil
 
-with open('data.bin', 'rb') as f:
+filename = sys.argv[1] + ".bin"
+
+with open(filename, 'rb') as f:
     data = f.read()
     dims = ceil(sqrt(len(data)))
     dims_bw = ceil(sqrt(len(data) * 8))
@@ -13,13 +16,24 @@ with open('data.bin', 'rb') as f:
     im_grayscale.putdata(data)
     im_grayscale.save('data_grayscale.png')
 
+    ones_count = 0
+    data_length = len(data)*8
+
     for x in range(len(data)):
         for z in range(8):
             idx = x * 8 + z
             a = idx//dims_bw
             b = idx % dims_bw
-            has_data = 255 if ((data[x] >> z) & 1) else 0
+
+            if ((data[x] >> z) & 1) == 1:
+                ones_count = ones_count + 1
+                has_data = 255
+            else:
+                has_data = 0
+
             im_data[a,b] = (0, has_data, 0)
+
+    print("Ones %: " + str((ones_count/data_length)*100))
 
     im_bw.save('data_bw.png')
 
